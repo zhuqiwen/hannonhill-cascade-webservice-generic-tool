ef368122f44baa400b9c35b0087a1676ea8c3cb6
Qiwen Zhu <qiwzhu@iu.edu>
2022-03-10 16:37:55 -0500

    add AssetFactory class

---
+++ src/Asset/AssetFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Edu\IU\Framework\GenericUpdater\Asset;
+
+class AssetFactory extends Asset{
+    public $assetTypeDisplay = "Asset Factory";
+    public $assetTypeFetch = ASSET_ASSET_FACTORY_FETCH;
+    public $assetTypeCreate = ASSET_ASSET_FACTORY_CREATE;
+}
