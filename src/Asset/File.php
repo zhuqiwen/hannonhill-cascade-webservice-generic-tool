e67033a8da36bc01d744552c7de09cddbb426f9f
Qiwen Zhu <qiwzhu@iu.edu>
2022-03-10 16:04:56 -0500

    add File class

--- src/Asset/File.php
+++ src/Asset/File.php
@@ -2,6 +2,17 @@
 
 namespace Edu\IU\Framework\GenericUpdater\Asset;
 
+use Edu\IU\Wcms\WebService\WCMSClient;
+
 class File extends Asset{
 
+
+    public function __construct(WCMSClient $wcms, string $path)
+    {
+        parent::__construct($wcms);
+        $this->assetTypeDisplay = "File";
+        $this->assetTypeFetch = ASSET_FILE_FETCH;
+        $this->assetTypeCreate = ASSET_FILE_CREATE;
+        $this->setOldAsset($path);
+    }
 }
--- src/Asset/FormatScript.php
+++ src/Asset/FormatScript.php
@@ -9,9 +9,9 @@ class FormatScript extends Asset{
     public function __construct(WCMSClient $wcms, string $path)
     {
         parent::__construct($wcms);
+        $this->assetTypeDisplay = "Velocity Format";
         $this->assetTypeCreate = ASSET_FORMAT_SCRIPT_CREATE;
         $this->assetTypeFetch = ASSET_FORMAT_SCRIPT_FETCH;
-
         $this->setOldAsset($path);
     }
 
