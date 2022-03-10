b19de7baec0219701ade9521146e409c77c5deca
Qiwen Zhu <qiwzhu@iu.edu>
2022-03-10 15:20:59 -0500

    add setOld/NewAsset() and assetExists() in Asset class

--- src/Asset/Asset.php
+++ src/Asset/Asset.php
@@ -4,8 +4,9 @@ namespace Edu\IU\Framework\GenericUpdater\Asset;
 
 
 use Edu\IU\Wcms\WebService\WCMSClient;
+use phpDocumentor\Reflection\Types\This;
 
-class Asset
+class Asset implements AssetInterface
 {
     use AssetTrait;
 
@@ -13,7 +14,30 @@ class Asset
     public function __construct(WCMSClient $wcms)
     {
         $this->wcms = $wcms;
+    }
 
+    public function setOldAsset(string $path)
+    {
+        if($this->assetExists($path))
+        {
+            $assetType = $this->assetTypeCreate;
+            $this->oldAsset = $this->wcms->fetchAsset($path, $this->assetTypeFetch)->$assetType;
+        }
+        else
+        {
+            //TODO: set error and throw exception for user api key
+        }
+    }
+
+    public function setNewAsset(\stdClass $assetData)
+    {
+        // TODO: Implement setNewAsset() method.
+        $this->newAsset = $assetData;
+    }
+
+    public function assetExists(string $path): bool
+    {
+        return $this->wcms->assetExists($path, $this->assetTypeFetch);
     }
 
 }
