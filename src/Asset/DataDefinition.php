914393f75f0c6eacbf57dfdbec684620ff7378ce
Qiwen Zhu <qiwzhu@iu.edu>
2022-03-10 16:43:49 -0500

    add DataDefinition class

---
+++ src/Asset/DataDefinition.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Edu\IU\Framework\GenericUpdater\Asset;
+
+
+class DataDefinition extends Asset{
+    public $assetTypeDisplay = "Data Definition";
+    public $assetTypeFetch = ASSET_DATA_DEFINITION_FETCH;
+    public $assetTypeCreate = ASSET_DATA_DEFINITION_CREATE;
+}
