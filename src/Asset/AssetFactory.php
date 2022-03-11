a919e44a7fb5d379c3921cc685a331f379c2100d
Qiwen Zhu <qiwzhu@iu.edu>
2022-03-11 10:11:03 -0500

    add udpateContent() in AssetFactory

--- src/Asset/AssetFactory.php
+++ src/Asset/AssetFactory.php
@@ -2,8 +2,13 @@
 
 namespace Edu\IU\Framework\GenericUpdater\Asset;
 
-class AssetFactory extends Asset{
+class AssetFactory extends Asset implements AssetInterface {
     public $assetTypeDisplay = "Asset Factory";
     public $assetTypeFetch = ASSET_ASSET_FACTORY_FETCH;
     public $assetTypeCreate = ASSET_ASSET_FACTORY_CREATE;
+
+    public function updateContent()
+    {
+        // TODO: Implement updateContent() method.
+    }
 }
