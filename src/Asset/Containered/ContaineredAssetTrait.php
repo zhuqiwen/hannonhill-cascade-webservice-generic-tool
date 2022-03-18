515009ae933ee448ed08f7f23768033d528c84c8
Qiwen Zhu <qiwzhu@iu.edu>
2022-03-18 15:12:36 -0400

    add ContainerdAssetTrait

---
+++ src/Asset/Containered/ContaineredAssetTrait.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Edu\IU\Framework\GenericUpdater\Asset\Containered;
+
+use Edu\IU\Framework\GenericUpdater\Exception\AssetNotFoundException;
+use phpDocumentor\Reflection\Types\This;
+
+trait ContaineredAssetTrait {
+
+    public function createAsset()
+    {
+        $parentClass = $this->getParentClass();
+        try {
+            new $parentClass($this->wcms, $this->newAsset->parentContainerPath);
+        }catch (AssetNotFoundException $e){
+            echo $e->getMessage() . ", which will be created now." . PHP_EOL;
+            $this->createParent();
+        }catch (\RuntimeException $e){
+            echo $e->getMessage();
+        }
+
+        if(!$this->assetExists($this->getNewAssetPath()))
+        {
+            unset($this->newAsset->path);
+            $this->wcms->createAsset($this->assetTypeCreate, $this->newAsset);
+            echo "The following Asset has been created:" . PHP_EOL;
+            echo "\tAsset Type: " . $this->assetTypeDisplay . PHP_EOL;
+            echo "\tAsset Path: " . $this->newAsset->parentContainerPath . DIRECTORY_SEPARATOR . $this->newAsset->name . PHP_EOL;
+        }
+    }
+
+    public function createParent()
+    {
+        $asset = $this->getGrantParentAssetForCreate();
+        $path = $asset->parentContainerPath . DIRECTORY_SEPARATOR . $asset->name;
+
+        if ($path == DIRECTORY_SEPARATOR)
+        {
+            return;
+        }
+
+        if(!$this->wcms->assetExists($path, $this->assetTypeFetch))
+        {
+            $parentClass = $this->getParentClass();
+            $folder = new $parentClass($this->wcms);
+            $folder->setNewAsset($asset);
+            $folder->createAsset();
+        }
+
+
+    }
+
+    public function getGrantParentAssetForCreate(): \stdClass
+    {
+        $array = explode(DIRECTORY_SEPARATOR, $this->newAsset->parentContainerPath);
+        $grantParentName = array_pop($array);
+        $grantParentPath = implode(DIRECTORY_SEPARATOR, $array);
+
+        return (object)[
+            'parentContainerPath' => empty($grantParentPath) ? '/' : $grantParentPath,
+            'name' => $grantParentName
+        ];
+
+    }
+
+    public function getParentClass(): string
+    {
+        $parentClass = get_parent_class(get_called_class());
+        if($parentClass == ROOT_CLASS_NAME)
+        {
+            $parentClass = get_called_class();
+        }
+
+        return $parentClass;
+    }
+}
