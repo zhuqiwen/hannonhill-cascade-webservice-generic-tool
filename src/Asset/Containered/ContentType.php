3f80f106a084f867900ac6fe608729745ffa3861
Qiwen Zhu <qiwzhu@iu.edu>
2022-03-19 21:06:13 -0400

    add checks for Content Type

--- src/Asset/Containered/ContentType.php
+++ src/Asset/Containered/ContentType.php
@@ -2,6 +2,7 @@
 
 namespace Edu\IU\Framework\GenericUpdater\Asset\Containered;
 
+use Edu\IU\Framework\GenericUpdater\Asset\WysiwygEditorConfiguration;
 use Edu\IU\Framework\GenericUpdater\Exception\InputIntegrityException;
 
 class ContentType extends ContentTypeContainer {
@@ -49,10 +50,43 @@ class ContentType extends ContentTypeContainer {
 
     public function checkDependencies(\stdClass $assetData)
     {
-        //TODO: check pageconfiguration set
-        //TODO: check metadata set
-        //TODO: if data definition path in payload, check it as well
 
+        $this->checkExistencePageConfigurationSet($assetData->pageConfigurationSetPath);
+        $this->checkExistenceMetadataSet($assetData->metadataSetPath);
+        //since Data Definition path is not required when creating a Content Type
+        if(isset($assetData->dataDefinitionPath) && !empty(trim($assetData->dataDefinitionPath))){
+            $this->checkExistenceDataDefinition($assetData->dataDefinitionPath);
+        }
+        //since editor configuration is not required when creating a Content Type
+        if(isset($assetData->editorConfigurationPath) && !empty(trim($assetData->editorConfigurationPath))){
+            $this->checkExistenceEditorConfiguration($assetData->editorConfigurationPath);
+        }
+
+
+    }
+
+    public function checkExistencePageConfigurationSet(string $path)
+    {
+        $asset = new PageConfigurationSet($this->wcms);
+        $this->checkExistenceAndThrowException($asset, $path);
+    }
+
+    public function checkExistenceMetadataSet(string $path)
+    {
+        $asset = new MetadataSet($this->wcms);
+        $this->checkExistenceAndThrowException($asset, $path);
+    }
+
+    public function checkExistenceDataDefinition(string $path)
+    {
+        $asset = new DataDefinition($this->wcms);
+        $this->checkExistenceAndThrowException($asset, $path);
+    }
+
+    public function checkExistenceEditorConfiguration(string $path)
+    {
+        $asset = new WysiwygEditorConfiguration($this->wcms);
+        $this->checkExistenceAndThrowException($asset, $path);
     }
 
 
