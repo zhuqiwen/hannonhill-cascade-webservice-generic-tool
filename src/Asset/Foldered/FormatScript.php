1d70ddea33c507c4cbd9e241288225bd0eff3aeb
Qiwen Zhu <qiwzhu@iu.edu>
2022-03-19 23:23:14 -0400

    add checks for FormatScript

--- src/Asset/Foldered/FormatScript.php
+++ src/Asset/Foldered/FormatScript.php
@@ -3,9 +3,26 @@
 namespace Edu\IU\Framework\GenericUpdater\Asset\Foldered;
 
 
+use Edu\IU\Framework\GenericUpdater\Exception\InputIntegrityException;
+
 class FormatScript extends Folder {
     public $assetTypeDisplay = "Velocity Format";
     public $assetTypeCreate = ASSET_FORMAT_SCRIPT_CREATE;
     public $assetTypeFetch = ASSET_FORMAT_SCRIPT_FETCH;
 
+
+    public function checkInputIntegrity(\stdClass $assetData)
+    {
+        parent::checkInputIntegrity($assetData);
+        $this->checkIfSetScript($assetData);
+    }
+
+    public function checkIfSetScript(\stdClass $asset)
+    {
+        if(!isset($asset->script) || empty(trim($asset->script))){
+            $msg = "For " . $this->assetTypeDisplay . " with path: " . $this->getNewAssetPath();
+            $msg .= ", [script] is required. Please add one by example: ";
+            throw new InputIntegrityException($msg);
+        }
+    }
 }
