b9f9050e5b6431516c802e8893cd939f72b7e18d
Qiwen Zhu <qiwzhu@iu.edu>
2022-03-21 00:04:57 -0400

    add check xml for Data Definition

--- src/Asset/Containered/DataDefinition.php
+++ src/Asset/Containered/DataDefinition.php
@@ -3,10 +3,49 @@
 namespace Edu\IU\Framework\GenericUpdater\Asset\Containered;
 
 
+use Edu\IU\Framework\GenericUpdater\Exception\InputIntegrityException;
+
 class DataDefinition extends DataDefinitionContainer {
     public $assetTypeDisplay = "Data Definition";
     public $assetTypeFetch = ASSET_DATA_DEFINITION_FETCH;
     public $assetTypeCreate = ASSET_DATA_DEFINITION_CREATE;
 
+    public function checkInputIntegrity(\stdClass $assetData)
+    {
+        parent::checkInputIntegrity($assetData);
+        $this->checkIfSetXML($assetData);
+        $this->checkIfValidXMLForDataDefinition($assetData->xml);
+    }
+
+
+
+    /**
+     * @param string $xml
+     * check if xml's root is <system-data-structure/>
+     */
+    public function checkIfValidXMLForDataDefinition(string $xml)
+    {
+        $xmlObj = simplexml_load_string($xml);
+        if($xmlObj->getName() != "system-data-structure"){
+            $msg = "For " . $this->assetTypeDisplay . " with path: " . $this->getNewAssetPath();
+            $msg .= ", the value of [xml] should have root node named '<system-data-structure/>'" .PHP_EOL;
+            throw new InputIntegrityException($msg);
+        }
+    }
+
+
+    public function checkDependencies(\stdClass $assetData)
+    {
+        parent::checkDependencies($assetData);
+        $this->checkExistencSharedFields($assetData->xml);
+
+    }
+
+    public function checkExistenceSharedFields(string $xml)
+    {
+        //TODO: get nodes of <shared-field>
+        //
+
+    }
 
 }
--- src/Asset/Foldered/Template.php
+++ src/Asset/Foldered/Template.php
@@ -16,37 +16,6 @@ class Template extends Folder {
         $this->checkIfSetXML($assetData);
     }
 
-    public function checkIfSetXML(\stdClass $asset)
-    {
-        [$isValidXML, $xmlErrors] = $this->isValidXML($asset->xml);
 
-        if(!isset($asset->xml) || empty(trim($asset->xml))){
-            $msg = "For " . $this->assetTypeDisplay . " with path: " . $this->getNewAssetPath();
-            $msg .= ", [xml] is required. Please add one by example: ";
-            throw new InputIntegrityException($msg);
-        }
 
-        if(!$isValidXML){
-            $msg = "For " . $this->assetTypeDisplay . " with path: " . $this->getNewAssetPath();
-            $msg .= ", the value of [xml] should be a valid xml." .PHP_EOL;
-            $msg .= "Errors: " . PHP_EOL;
-            $msg .= "\t" . print_r($xmlErrors);
-            throw new InputIntegrityException($msg);
-        }
-    }
-
-    public function isValidXML(string $xml): array
-    {
-        $prev = libxml_use_internal_errors(true);
-
-        $doc = simplexml_load_string($xml);
-        $errors = libxml_get_errors();
-
-        libxml_clear_errors();
-        libxml_use_internal_errors($prev);
-
-        $result =  false !== $doc && empty($errors);
-
-        return compact('result', 'errors');
-    }
 }
