ac46a1b9afeb655dbf2208fe322485a38be25f2a
Qiwen Zhu <qiwzhu@iu.edu>
2024-09-12 16:45:39 -0400

    decode based64 string in data attribute for File asset, so that the content of file won't be broken

--- src/Asset/Foldered/File.php
+++ src/Asset/Foldered/File.php
@@ -28,4 +28,17 @@ class File extends Folder {
             throw new InputIntegrityException($msg);
         }
     }
+
+    public function setNewAsset(\stdClass $assetData): void
+    {
+        //data attribute has to be base64 encoded when create File object, so that the json_decode(json_encode()) process won't return null
+        //However, when passing data to wcms via SOAP, the data attribute has to be base64Binary, instead of base64 string.
+        //so, check if data attribute is base65 encoded
+        $dataBase64Decoded = base64_decode($assetData->data, true);
+        if ($dataBase64Decoded !== false){
+            $assetData->data = $dataBase64Decoded;
+        }
+
+        parent::setNewAsset($assetData);
+    }
 }
