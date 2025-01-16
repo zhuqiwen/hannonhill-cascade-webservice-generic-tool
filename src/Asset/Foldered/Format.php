352dd34cbe873c9e4bcf70bfde73e12d5ff325d3
Qiwen Zhu <qiwzhu@iu.edu>
2025-01-16 13:17:11 -0500

    disable traits in Format to avoid oop errors

--- src/Asset/Foldered/Format.php
+++ src/Asset/Foldered/Format.php
@@ -10,29 +10,29 @@ class Format extends Folder{
     //To disable metadata traits for format and velocity format
     // set all trait methods as private
     use MetadataTraits{
-        getOldMetadataByName as private;
-        getOldMetadataAuthor as private;
-        getOldMetadataDisplayName as private;
-        getOldMetadataEndDate as private;
-        getOldMetadataKeywords as private;
-        getOldMetadataDescription as private;
-        getOldMetadataReviewDate as private;
-        getOldMetadataStartDate as private;
-        getOldMetadataSummary as private;
-        getOldMetadataTeaser as private;
-        getOldMetadataTitle as private;
-        getOldMetadataDynamicFieldByName as private;
-        getNewMetadataDynamicFieldByName as private;
-        getOldMetadataDynamicFields as private;
-        getNewMetadataDynamicFields as private;
-        setNewMetadataDynamicField as private;
-        convertDynamicFields2Array as private;
-        convertArray2DynamicFields as private;
+//        getOldMetadataByName as private;
+//        getOldMetadataAuthor as private;
+//        getOldMetadataDisplayName as private;
+//        getOldMetadataEndDate as private;
+//        getOldMetadataKeywords as private;
+//        getOldMetadataDescription as private;
+//        getOldMetadataReviewDate as private;
+//        getOldMetadataStartDate as private;
+//        getOldMetadataSummary as private;
+//        getOldMetadataTeaser as private;
+//        getOldMetadataTitle as private;
+//        getOldMetadataDynamicFieldByName as private;
+//        getNewMetadataDynamicFieldByName as private;
+//        getOldMetadataDynamicFields as private;
+//        getNewMetadataDynamicFields as private;
+//        setNewMetadataDynamicField as private;
+//        convertDynamicFields2Array as private;
+//        convertArray2DynamicFields as private;
     }
     use TagsTraits{
-        getOldTags as private;
-        getNewTags as private;
-        setNewTags as private;
-        addNewTag as private;
+//        getOldTags as private;
+//        getNewTags as private;
+//        setNewTags as private;
+//        addNewTag as private;
     }
 }
