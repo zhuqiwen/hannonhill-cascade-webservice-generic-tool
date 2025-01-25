b19a6b2c5cb7a3712c086152741ddf68828f73eb
Qiwen Zhu <qiwzhu@iu.edu>
2025-01-25 09:51:06 -0500

    add getters in PageConfigurationSet.php to get PageConfiguration by template path, extension, serialization types, name, etc

--- src/Asset/Containered/PageConfigurationSet.php
+++ src/Asset/Containered/PageConfigurationSet.php
@@ -83,5 +83,130 @@ PAGECONFIGEXAMPLE;
     }
 
 
+    /**
+     * get $asset->pageConfigurations->pageConfiguration
+     * if there is only one pageConfiguration, then put it in a array
+     * @return array
+     */
+    public function getPageConfigurations(): array
+    {
+        $result = $this->getOldAsset()->pageConfigurations->pageConfiguration;
+        if(!is_array($result) && $result instanceof \stdClass){
+            $result = [$result];
+        }
+
+        return $result;
+    }
+
+
+    public function getPageConfigurationByName(string $pageConfigurationName):\stdClass | null
+    {
+        $result = null;
+        $pageConfigurations = $this->getPageConfigurations();
+        foreach ($pageConfigurations as $pageConfiguration){
+            if($pageConfiguration->name == $pageConfigurationName){
+                $result = $pageConfiguration;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * get an array of $asset->pageConfigurations->pageConfiguration by checking $asset->pageConfigurations->pageConfiguration->templatePath;
+     * if none, return empty array
+     * @param string $templatePath
+     * @return array
+     */
+    public function getPageConfigurationsByTemplatePath(string $templatePath): array
+    {
+        $result = [];
+        $pageConfigurations = $this->getPageConfigurations();
+        foreach ($pageConfigurations as $pageConfiguration){
+            if($pageConfiguration->templatePath == $templatePath){
+                $result[] = $pageConfiguration;
+            }
+        }
+    }
+
+    /**
+     * Retrieve page configurations filtered by the specified output extension.
+     *
+     *  get an array of $asset->pageConfigurations->pageConfiguration by checking $asset->pageConfigurations->pageConfiguration->outputExtension;
+     *  if none, return empty array
+     * @param string $outputExtension The output extension to filter page configurations by.
+     * @return array An array of page configurations matching the given output extension.
+     */
+    public function getPageConfigurationsByOutputExtension(string $outputExtension): array
+    {
+        $result = [];
+        $pageConfigurations = $this->getPageConfigurations();
+        foreach ($pageConfigurations as $pageConfiguration){
+            if($pageConfiguration->outputExtension == $outputExtension){
+                $result[] = $pageConfiguration;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * get an array of $asset->pageConfigurations->pageConfiguration by checking $asset->pageConfigurations->pageConfiguration->serializationType;
+     * if none, return empty array
+     *
+     * @param string $serializationType
+     * @return array
+     */
+    public function getPageConfigurationsBySerializationType(string $serializationType): array
+    {
+        $result = [];
+        $pageConfigurations = $this->getPageConfigurations();
+        foreach ($pageConfigurations as $pageConfiguration){
+            if($pageConfiguration->serializationType == $serializationType){
+                $result[] = $pageConfiguration;
+            }
+        }
+
+        return $result;
+
+    }
+
+    /**
+     *
+     * get an array of $asset->pageConfigurations->pageConfiguration
+     * by checking if $asset->pageConfigurations->pageConfiguration->publishable is true;
+     *  if none, return empty array
+     * @return array
+     */
+    public function getPublishablePageConfigurations(): array
+    {
+        $result = [];
+        $pageConfigurations = $this->getPageConfigurations();
+        foreach ($pageConfigurations as $pageConfiguration){
+            if($pageConfiguration->publishable == '1'){
+                $result[] = $pageConfiguration;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * get the default pageConfiguration
+     * @return \stdClass|null
+     */
+    public function getDefaultPageConfiguration(): \stdClass | null
+    {
+        $result = null;
+        $pageConfigurations = $this->getPageConfigurations();
+        foreach ($pageConfigurations as $pageConfiguration){
+            if($pageConfiguration->defaultConfiguration == '1'){
+                $result = $pageConfiguration;
+            }
+        }
+
+        return $result;
+
+    }
 
 }
